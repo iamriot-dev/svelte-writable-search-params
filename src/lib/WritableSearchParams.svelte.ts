065112f3eef1c8825f66bfd22e5f2c5bdf6d3351
iamriot-dev <59467725+iamriot-dev@users.noreply.github.com>
2026-07-06 23:26:39 +0800

    fix types

--- src/lib/WritableSearchParams.svelte.ts
+++ src/lib/WritableSearchParams.svelte.ts
@@ -2,6 +2,42 @@ import type { StandardSchemaV1 } from "@standard-schema/spec";
 import { onMount, untrack } from "svelte";
 import { SvelteURLSearchParams } from "svelte/reactivity";
 
+interface BaseWritableSearchParams {
+	readonly current: SvelteURLSearchParams;
+}
+
+interface CreateStateFn {
+	<const S extends StandardSchemaV1<string | null | undefined, unknown>>(
+		key: string,
+		schema: S,
+		fallback: StandardSchemaV1.InferOutput<S>,
+		config?: {
+			encoder?: (value: StandardSchemaV1.InferOutput<S>) => string;
+		},
+	): {
+		value: StandardSchemaV1.InferOutput<S>;
+	};
+}
+
+interface CreateStateAsyncFn {
+	<const S extends StandardSchemaV1<string | null | undefined, unknown>>(
+		key: string,
+		schema: S,
+		fallback: StandardSchemaV1.InferOutput<S>,
+		config?: {
+			encoder?: (value: StandardSchemaV1.InferOutput<S>) => string;
+		},
+	): {
+		get value(): Promise<StandardSchemaV1.InferOutput<S>>;
+		set value(newVal: StandardSchemaV1.InferOutput<S>);
+	};
+}
+
+export interface WritableSearchParams extends BaseWritableSearchParams {
+	createStateFor: CreateStateFn;
+	createStateFor_async: CreateStateAsyncFn;
+}
+
 const SYM = Symbol("WritableSearchParams");
 
 type Init = NonNullable<ConstructorParameters<typeof URLSearchParams>[0]>;
